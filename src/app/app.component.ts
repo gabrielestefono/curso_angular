a0616d2d9e2a283451dea8b37dae8ed4e1a7965a
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-29 11:10:29 -0300

    ngOnDestroy (Não funcionou!)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
+  <app-title *ngIf="destruir"></app-title>
+  <br>
+  <button (click)="destruirComponent()">Destruir Componente</button>
+  <br>
+  <br>
   {{valor}}
   <button (click)="adicionar()">Adicionar</button>
   <router-outlet></router-outlet>
   `,
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
   public valor = 1;
+
+  public destruir:boolean = true;
 
   public adicionar(){
     return this.valor = ++this.valor;
+  }
+
+  public destruirComponent(){
+    return this.destruir = false;
   }
 
   ngOnInit(): void {console.log('ngOnInit')}
@@ -26,4 +37,6 @@ export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterCon
   ngAfterViewInit(): void {console.log('ngAfterViewInit')}
 
   ngAfterViewChecked(): void {console.log('ngAfterViewChecked')}
+
+  ngOnDestroy(): void {console.log('Destruído')}
 }
